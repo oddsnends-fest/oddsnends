e3cb1634b5ee69919e461e67872cf7e94be9b471
Passawut <passawut.prinyagul@gmail.com>
2025-01-16 23:28:07 +0700

    fix: add optional to mentioned schema fields

--- src/server/validate/user.validate.ts
+++ src/server/validate/user.validate.ts
@@ -4,13 +4,14 @@ export const User = z.object({
   user_id: z.string(),
   display_name: z.string(),
   full_name: z.string(),
-  line_profile_pic: z.string().url("Invalid URL"),
-  phone: z.string().regex(/^[0-9]{9,10}$/, "Phone number isn't 9 or 10 digit long"),
-  gender: z.enum(["MALE", "FEMALE", "NONBINARY", "OTHERS", "PREFERNOTTOSAY"]),
-  photo: z.string(),
+  line_profile_pic: z.string().url("Invalid URL").optional(),
+  phone: z.string().regex(/^[0-9]{9,10}$/, "Phone number isn't 9 or 10 digit long").optional(),
+  gender: z.enum(["MALE", "FEMALE", "NONBINARY", "OTHERS", "PREFERNOTTOSAY"]).optional(),
+  photo: z.string().optional(),
   age: z.number().min(0, "Invalid age"),
   occupation: z.string(),
   email: z.string().email("Invalid email format"),
   channel: z.enum(["INSTAGRAM", "FACEBOOK", "TIKTOK", "LINE", "FRIENDS", "POSTER"])
 });
+
 
