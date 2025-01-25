91821d55feb73e8c5d4d956b22f4ed2738ece395
LittleNa1000 <naphanccnp@gmail.com>
2025-01-25 21:29:22 +0700

    feat: add calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function HomePage() {
             <h3 className="text-2xl font-bold">First Steps →</h3>
             <div className="text-lg">
               Just the basics - Everything you need to know to set up your
-              database and authentication.
             </div>
           </Link>
           <Link
