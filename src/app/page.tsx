4d609e788003729b9af11490d7701932c5c74dc4
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-01-26 17:22:12 +0700

    fix: test overflow in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+    <>
+      <div className="flex flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] gap-12 px-4 py-16 text-white">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
         </h1>
@@ -31,7 +31,14 @@ export default function HomePage() {
             </div>
           </Link>
         </div>
+        <p>Test</p>
+        <p>Overflow</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
       </div>
-    </main>
+    </>
   );
 }
