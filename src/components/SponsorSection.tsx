2ee10cb72cdddcd08748212e19b29823199ceb58
sunnnnn <putawan.sra@gmail.com>
2025-01-28 06:54:34 +0700

    fix: spacing

--- src/components/SponsorSection.tsx
+++ src/components/SponsorSection.tsx
@@ -1,14 +1,23 @@
 import Image from "next/image"
 
+const sponsers: string[] = [
+  "/",
+  "/",
+  "/",
+  "/",
+  "/",
+  "/",
+]
+
 const SponsorSection = () => {
   return (
-    <div className="mb-6 w-screen flex flex-row justify-center gap-5">
-      <Image src="/" alt="" width={35} height={35} className="border"/>
-      <Image src="/" alt="" width={35} height={35} className="border"/>
-      <Image src="/" alt="" width={35} height={35} className="border"/>
-      <Image src="/" alt="" width={35} height={35} className="border"/>
-      <Image src="/" alt="" width={35} height={35} className="border"/>
-      <Image src="/" alt="" width={35} height={35} className="border"/>    
+    <div className="mb-6 flex flex-row justify-center items-center gap-5">
+      {/* Map through Sponsers image's src */}
+      {sponsers.map((src, idx) => (
+        <div key={idx}>
+          <Image src={src} alt={`${idx+1}`} width={35} height={35} className="border"/>
+        </div>
+      ))}
     </div>
   )
 }
