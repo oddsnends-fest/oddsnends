9c64eb9e1730ddf6001e6e3d5ace6eaca180f954
wnmay <wanathachat@gmail.com>
2025-01-31 13:10:16 +0700

    feat: adjust layout

--- src/app/photoid/fill-in/page.tsx
+++ src/app/photoid/fill-in/page.tsx
@@ -1,20 +1,21 @@
-"use client"
+"use client";
 import { useState, FormEvent } from "react";
 import { Calendar } from "@/components/ui/calendar";
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 import React from "react";
+import Image from "next/image";
 
 export default function FormPage() {
   const [name, setName] = useState("");
   const [hobby, setHobby] = useState("");
-  const [date, setDate] = React.useState<Date>()
+  const [date, setDate] = React.useState<Date>();
   const [spiritAnimal, setSpiritAnimal] = useState("");
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -29,12 +30,21 @@ export default function FormPage() {
   };
 
   return (
-    <>
-    <div className="text-6xl font-bold text-center m-6">Fill in</div>
-        <form onSubmit={handleSubmit} className="grid gap-4 m-4">
-          <div className="grid grid-cols-2 gap-4">
+    <div className="h-screen overflow-hidden">
+      <Image
+        src="/"
+        alt="LogoImage"
+        width={60}
+        height={60}
+        className="mx-auto mt-14 border"
+      />
+      <div className="mt-5 flex justify-center text-center text-4xl font-bold">
+        Fill in
+      </div>
+      <form onSubmit={handleSubmit} className="m-6 mt-20 grid gap-4">
+        <div className="grid grid-cols-2 gap-8">
           <div>
-            <label htmlFor="name" className="block text-md font-medium mb-2">
+            <label htmlFor="name" className="mb-2 text-xl font-medium">
               Name
             </label>
             <input
@@ -42,88 +52,100 @@ export default function FormPage() {
               placeholder="Enter your name"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="p-2 block h-10 w-full rounded-md border-2 border-black sm:text-sm"
+              className="h-8 w-full rounded-lg border-2 border-black p-2 text-xs"
             />
           </div>
 
           <div>
-            <label htmlFor="hobby" className="block text-md font-medium mb-2">
+            <label htmlFor="hobby" className="mb-2 text-xl font-medium">
               Hobby
             </label>
             <select
               id="hobby"
-              className={`p-2 h-10 block w-full rounded-md border-2 border-black sm:text-sm ${hobby=="" ? "text-gray-400" : "text-black"}`}
+              className={`h-8 w-full rounded-md border-2 border-black p-2 text-xs ${hobby == "" ? "text-gray-400" : "text-black"}`}
               value={hobby}
               onChange={(e) => setHobby(e.target.value)}
-              
             >
-              <option value="" disabled className="text-gray-400">Select. . .</option>
-              <option value="a" className="text-black">A</option>
-              <option value="b" className="text-black">B</option>
-              <option value="c" className="text-black">C</option>
+              <option value="" disabled className="text-gray-400">
+                Select. . .
+              </option>
+              <option value="a" className="text-black">
+                A
+              </option>
+              <option value="b" className="text-black">
+                B
+              </option>
+              <option value="c" className="text-black">
+                C
+              </option>
             </select>
           </div>
 
           <div>
-  <label htmlFor="dob" className="block text-md font-medium mb-2">
-    Date of birth
-  </label>
-    <div className="flex items-center gap-2">
-    <div
-      className={`p-2 block w-full rounded-md border-2 border-black sm:text-sm 
-        ${date ? "text-black" : "text-gray-400"}`}
-    >
-      {date ? format(date, "dd/MM/yyyy") : "DD/MM/YYYY"}
-    </div>
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className="w-10 h-10 p-0 flex items-center justify-center border-2 border-black"
-        >
-          <CalendarIcon className="h-5 w-5" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0" align="end">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={(selectedDate) => selectedDate && setDate(selectedDate)}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
-    
-  </div>
-</div>
-
+            <label htmlFor="date" className="mb-2 text-xl font-medium">
+              Date of birth
+            </label>
+            <div className="flex items-center gap-2">
+              <div
+                className={`h-8 w-full rounded-md border-2 border-black p-2 text-xs ${date ? "text-black" : "text-gray-400"}`}
+              >
+                {date ? format(date, "dd/MM/yyyy") : "DD/MM/YYYY"}
+              </div>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    className="flex h-8 w-10 items-center justify-center border-2 border-black p-0"
+                  >
+                    <CalendarIcon className="h-5 w-5" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="end">
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={(selectedDate) =>
+                      selectedDate && setDate(selectedDate)
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+          </div>
 
           <div>
-            <label htmlFor="spirit-animal" className="block text-md font-medium mb-2">
+            <label htmlFor="spirit-animal" className="mb-2 text-xl font-medium">
               Spirit Animal
             </label>
             <select
               id="spirit-animal"
-              className={`h-10 p-2 block w-full rounded-md border-2 border-black sm:text-sm ${spiritAnimal=="" ? "text-gray-400" : "text-black"}`}
+              className={`h-8 w-full rounded-md border-2 border-black p-2 text-xs ${spiritAnimal == "" ? "text-gray-400" : "text-black"}`}
               value={spiritAnimal}
               onChange={(e) => setSpiritAnimal(e.target.value)}
             >
-              <option value="" disabled className="text-gray-400">Select. . .</option>
-              <option value="d" className="text-black">D</option>
-              <option value="e" className="text-black">E</option>
-              <option value="f" className="text-black">F</option>
+              <option value="" disabled className="text-gray-400">
+                Select. . .
+              </option>
+              <option value="d" className="text-black">
+                D
+              </option>
+              <option value="e" className="text-black">
+                E
+              </option>
+              <option value="f" className="text-black">
+                F
+              </option>
             </select>
           </div>
-          </div>
-
-          <Button
-            type="submit"
-            className="bg-black text-white py-2 rounded-xl hover:bg-gray-500"
-          >
-            Submit
-          </Button>
-        </form>
-    </>
-   
+        </div>
+        <Button
+          type="submit"
+          className="mx-auto w-1/2 rounded-xl bg-black p-3 text-white hover:bg-gray-500"
+        >
+          Send
+        </Button>
+      </form>
+    </div>
   );
 }
