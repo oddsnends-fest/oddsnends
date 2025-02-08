4032cacfef974f40335e21c9f524ffcb3282c972
Max <maxxxx0129@gmail.com>
2025-02-08 18:20:29 +0700

    styling

--- src/app/components/PhotoIDCard/PhotoIDCard.tsx
+++ src/app/components/PhotoIDCard/PhotoIDCard.tsx
@@ -7,41 +7,53 @@ import tyler from 'src/app/components/PhotoIDCard/tyler.png'
 // 3.  เส้น______ ทำให้เป็นเส้นดีๆ
 const PhotoIDCard = () => {
   return (
-    <div className='bg-amber-200'> 
-        <div className='flex w-3/4 bg-lime-400 gap-3 m-auto ' >
-            <div className='w-1/4'>
+    <div> 
+        <div className='flex w-full bg-amber-200 gap-3 m-auto rounded-t-lg ' >
+            <div className='w-1/3'>
                 <img src = {tyler.src} className='w-full h-4/5 m-4' />
             </div>
-            <div className='w-3/4 m-4'>
-                <div className='flex'> 
+            <div className='w-2/3 m-4'>
+                <div className='flex '> 
                     <div className='w-3/4'>
-                        <p>name___________</p>
+                        <div className='flex items-baseline gap-2 mt-3'>
+                            <p>Name</p>
+                            <p className='flex-1 border-b border-black'>P'Fame</p>
+                        </div>
                     </div>
                     <div className='w-1/4'>
                         <img src = {tyler.src} className='w-full h-auto'/>
                     </div>
                 </div>
                 <div>
-                    <p>date of birth________</p>
-                    <p>hobby_______</p>
-                    <p>spirit animal_________</p>
+                        <div className='flex items-baseline gap-2 mt-3'>
+                            <p>Date of birth</p>
+                            <p className='flex-1 border-b border-black'>25 yr ago</p>
+                        </div>
+                        <div className='flex items-baseline gap-2 mt-3'>
+                            <p>Hobbby</p>
+                            <div className='flex-1 border-b border-black'></div>
+                        </div>
+                        <div className='flex items-baseline gap-2 mt-3'>
+                            <p>Spirit Animal</p>
+                            <div className='flex-1 border-b border-black'></div>
+                        </div>
                 </div>
             </div>
         </div>
-        <div className='flex gap-2 bg-lime-400 w-3/4 m-auto'>
-            <div className='m-2'>
-                Date of issue ________
-            </div>
-            <div className='m-2'>
-                <div>
-                    ____________
-                </div>
-                <div>
-                    signature
-                </div>
-                
-            </div>
-        </div>
+<div className="flex bg-amber-200 justify-between items-center rounded-b-lg">
+  {/* Date of Issue Section */}
+  <div className="flex items-center gap-2">
+    <p>Date of issue</p>
+    <div className="w-32 border-b border-black"></div>
+  </div>
+
+  {/* Signature Section */}
+  <div className="flex flex-col items-center">
+    <div className="w-32 border-b border-black"></div>
+    <p className="text-sm">Signature</p>
+  </div>
+</div>
+
     </div>
   )
 }
