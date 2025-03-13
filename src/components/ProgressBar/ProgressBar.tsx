c4c2f5d6ba436855373ead32e3ec028486bca1cd
wnmay <wanathachat@gmail.com>
2025-03-13 15:40:58 +0700

    fix: use coalescing operator for handling progress level

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,11 +1,10 @@
 import { Progress } from "../ui/progress";
 
-function ProgressBar({ progressLevel }: { progressLevel?: number }) {
-  const progress = progressLevel ?? 0;
-  if (progress < 0 || progress > 1) {
+function ProgressBar({ progressLevel }: { progressLevel: number }) {
+  if ((progressLevel ?? 0) < 0 || (progressLevel ?? 0) > 1) {
     console.warn("progressLevel must be between 0 and 1");
   }
-  const acceptedProgress = Math.max(0, Math.min(1, progress));
+  const acceptedProgress = Math.max(0, Math.min(1, progressLevel ?? 0));
   return <Progress value={acceptedProgress * 100} />;
 }
 
