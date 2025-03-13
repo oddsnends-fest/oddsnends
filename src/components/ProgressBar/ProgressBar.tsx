5e6d4e98b36175f56e7c976b06c483c984b20572
wnmay <wanathachat@gmail.com>
2025-03-13 16:01:00 +0700

    fix: use AND operator instead of OR

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,10 +1,13 @@
 import { Progress } from "../ui/progress";
 
 function ProgressBar({ progressLevel }: { progressLevel: number }) {
-  if ((progressLevel ?? 0) < 0 || (progressLevel ?? 0) > 1) {
+  const progress = progressLevel ?? 0;
+
+  if (!(progress >= 0 && progress <= 1)) {
     console.warn("progressLevel must be between 0 and 1");
   }
-  const acceptedProgress = Math.max(0, Math.min(1, progressLevel ?? 0));
+
+  const acceptedProgress = Math.max(0, Math.min(1, progress));
   return <Progress value={acceptedProgress * 100} />;
 }
 
