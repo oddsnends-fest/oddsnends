9d0618edad3d970bf8fc6aad3f2512c1a75bbc5b
LittleNa1000 <naphanccnp@gmail.com>
2025-03-19 02:10:59 +0700

    fix: styling

--- src/components/SponsorSection/SponsorSection.tsx
+++ src/components/SponsorSection/SponsorSection.tsx
@@ -1,38 +1,42 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const sponsors = [
-  { src: "/images/sponsors/SlowCombo.png", link: "https://www.instagram.com/slowcombo/?hl=en", size: 110},
-  { src: "/images/sponsors/IOIC.png", link: "https://www.instagram.com/ioic_chula/?hl=en", size: 95},
-//   { src: "/", link: "/"},
-//   { src: "/", link: "/"},
-//   { src: "/", link: "/"},
-//   { src: "/", link: "/"},
-]
+  {
+    src: "/images/sponsors/SlowCombo.png",
+    link: "https://www.instagram.com/slowcombo/?hl=en",
+    size: 110,
+  },
+  {
+    src: "/images/sponsors/IOIC.png",
+    link: "https://www.instagram.com/ioic_chula/?hl=en",
+    size: 95,
+  },
+  //   { src: "/", link: "/"},
+  //   { src: "/", link: "/"},
+  //   { src: "/", link: "/"},
+  //   { src: "/", link: "/"},
+];
 
 const SponsorSection = () => {
   return (
-    <section className="mb-12 bg-transparent z-10">
-        <ul className="flex flex-wrap justify-center items-end ">
-            {sponsors.map(({ src, link, size }, idx) => (
-                <li key={idx}>
-                    <a 
-                        href={link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image 
-                            src={src} 
-                            alt={`${idx + 1}`} 
-                            width={size} 
-                            height={size}
-                            className=""
-                        />
-                    </a>
-                </li>
-            ))}
-        </ul>
-  </section>
-  )
-}
+    <section className="z-10 mb-14 bg-transparent">
+      <ul className="flex flex-wrap items-end justify-center">
+        {sponsors.map(({ src, link, size }, idx) => (
+          <li key={idx}>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              <Image
+                src={src}
+                alt={`${idx + 1}`}
+                width={size}
+                height={size}
+                className=""
+              />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
-export default SponsorSection
+export default SponsorSection;
