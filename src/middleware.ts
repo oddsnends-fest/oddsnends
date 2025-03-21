d1d7e5a40abe5cee409e552533e0e6662a0fe836
Passawut <passawut.prinyagul@gmail.com>
2025-03-22 00:15:28 +0700

    fix: type security

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,23 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { VerifiedToken } from './types/apiModel';
 
 export async function middleware(request: NextRequest) {
 
     // Extract the ID token from the Authorization header
     const idtoken = request.headers.get('Authorization')?.split('Bearer ')[1];
+    
     if (!idtoken) {
         return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
     try {
+
+        // Check if the LINE_LOGIN_CHANNEL_ID is set
+        if (!process.env.LINE_LOGIN_CHANNEL_ID) {
+            return NextResponse.json({ message: "Unauthorized - LINE_LOGIN_CHANNEL_ID is not set in the environment variables." }, { status: 401 });
+        }
+
         // Verify the ID token with LINE Login API
         const response = await fetch('https://api.line.me/oauth2/v2.1/verify', {
             method: 'POST',
@@ -18,7 +26,7 @@ export async function middleware(request: NextRequest) {
             },
             body: new URLSearchParams({
                 id_token: idtoken,
-                client_id: process.env.LINE_LOGIN_CHANNEL_ID || '',
+                client_id: process.env.LINE_LOGIN_CHANNEL_ID,
             }),
         });
         
@@ -28,13 +36,13 @@ export async function middleware(request: NextRequest) {
             return NextResponse.json({ message: "Unauthorized", error: errorData }, { status: 401 });
         }
 
-        const { name, picture, email, ...other } = await response.json();
+        const responseJson: VerifiedToken = await response.json();
         const res = NextResponse.next();
 
         // Set user details in custom headers
-        res.headers.set("X-User-Name", name);
-        res.headers.set("X-User-Picture", picture);
-        res.headers.set("X-User-Email", email);
+        res.headers.set("X-User-Name", responseJson.name);
+        res.headers.set("X-User-Picture", responseJson.picture);
+        res.headers.set("X-User-Email", responseJson.email);
 
         return res;
     }
