794a72e768a5ac7555f4af32d88e384c6d6a6682
Jay <j_jayjay@hotmail.com>
2025-03-23 13:56:55 +0700

    fix: fix await params error refactor: rename api/users/[id].ts to api/users/[id]/route.ts

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -6,9 +6,11 @@ interface Params {
     id: string;
 }
 
-export async function GET(req: NextRequest, { params }: { params: Params }) {
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
     try {
-        const userId = params.id; // Get user ID from the dynamic route
+      const params = context.params; // Get user id from dynamic route
+      const { id } = await params;
+      const userId = id;
 
         if (!userId) {
             return NextResponse.json({ error: "User ID is required" }, { status: 400 });
