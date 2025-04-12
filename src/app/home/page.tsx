b05d516b0625543f75c4120c9af230dfc5875ee9
etarth <134919103+etarth@users.noreply.github.com>
2025-04-12 11:50:40 +0700

    fix: merge conflict

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,63 +1,66 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import SlidingBox from "@/components/SlidingBox/SlidingBox";
 import { useLiff } from "@/providers/liff-provider";
-import { useState, useEffect, createContext, useContext, type ReactNode } from "react";
 
 interface UserProfile {
-    displayName: string; 
-    pictureUrl?: string; 
-    userId: string
-  }
+  displayName: string;
+  pictureUrl?: string;
+  userId: string;
+}
 
 export default function HomePage() {
+  const { liff, liffError, idToken, userProfile } = useLiff();
 
-    const { liff, liffError, idToken, userProfile } = useLiff();
+  return (
+    <>
+      {/* Background */}
+      <div
+        className="absolute top-0 -z-10 h-full w-full bg-cover bg-center"
+        style={{ backgroundImage: "url('images/background/home.png')" }}
+      />
 
-    return (
-        <>  
-            {/* Background */}
-            <div className="absolute top-0 h-full w-full bg-center bg-cover -z-10"
-                style={{ backgroundImage: "url('images/background/home.png')"}}
-            />
-            
-            {/* content */}
-            <main className="mt-5 flex flex-col justify-center items-center gap-8">
-                {/* Title */}
-                <div className="text-center text-white drop-shadow-[0px_2px_1px_rgba(0,0,0,0.3)] leading-[1.75]">
-                    <h1 className="text-[1.8rem] tracking-[0.1em] font-cooper">Sandbox Space</h1>
-                    <div className="font-semibold text-[0.9rem] px-2 tracking-wide">FOR ARTISTS, ENTREPRENEURS, AND BEYOND</div>
-                </div>
+      {/* content */}
+      <main className="mt-5 flex flex-col items-center justify-center gap-8">
+        {/* Title */}
+        <div className="text-center leading-[1.75] text-white drop-shadow-[0px_2px_1px_rgba(0,0,0,0.3)]">
+          <h1 className="font-cooper text-[1.8rem] tracking-[0.1em]">
+            Sandbox Space
+          </h1>
+          <div className="px-2 text-[0.9rem] font-semibold tracking-wide">
+            FOR ARTISTS, ENTREPRENEURS, AND BEYOND
+          </div>
+        </div>
 
-                {/* Sliding Box */}
-                <SlidingBox />
+        {/* Sliding Box */}
+        <SlidingBox />
 
-                {/* Buttons */}
-                <div className="relative mb-16 flex flex-col justify-center items-center drop-shadow-[0_0_70px_rgba(218,188,144,1)]">
-                    <Image 
-                        src="/images/oddie.png"
-                        alt="oddie.png"
-                        width={90}
-                        height={90}
-                        className="z-10 drop-shadow-[0_5px_3px_rgba(0,0,0,0.3)]"
-                    />
-                    <button 
-                        type="button"
-                        className="absolute top-[75px] py-4 w-64 rounded-full bg-purple-gradient text-white text-xl font-semibold tracking-wider drop-shadow-[0_5px_3px_rgba(0,0,0,0.3)]"
-                    >
-                        <a
-                            href="https://docs.google.com/forms/d/e/1FAIpQLScpRDeY39gJaVQ1WHkfvKt-1vo8xnPYnuBazcNZsO7pV799EA/viewform"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            DIY COOKIE
-                        </a>
-                    </button>
-                </div>
-            </main>
+        {/* Buttons */}
+        <div className="relative mb-16 flex flex-col items-center justify-center drop-shadow-[0_0_70px_rgba(218,188,144,1)]">
+          <Image
+            src="/images/oddie-2.png"
+            alt="oddie.png"
+            width={90}
+            height={90}
+            className="z-10 drop-shadow-[0_5px_3px_rgba(0,0,0,0.3)]"
+          />
+          <button
+            type="button"
+            className="absolute top-[75px] w-64 rounded-full bg-purple-gradient py-4 text-xl font-semibold tracking-wider text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.3)]"
+          >
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdVmKSrSb4eHuxhudDcDosVWejYXvboW3Perl6z3a0zD-HPDA/viewform"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              JOIN OUR <span className="text-[#FFDF58]">WORKSHOP!</span>
+            </a>
+          </button>
+        </div>
+      </main>
 
-            <div className="absolute bottom-0 h-96 w-full bg-black -z-20" />
-        </>
-    );
+      <div className="absolute bottom-0 -z-20 h-96 w-full bg-black" />
+    </>
+  );
 }
