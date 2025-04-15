de03f9507959b50f91240a84de97c40cad8e8481
future-html <witsanuporn19646@gmail.com>
2025-04-15 13:44:39 +0700

    fix: image url signature page

--- src/app/signature/page.tsx
+++ src/app/signature/page.tsx
@@ -1,9 +1,10 @@
 import Signature from "@/components/Signature";
-
+import { useState } from "react";
 export default function SignaturePage() {
+  const [base64ImageUrl, setbase64ImageUrl] = useState<string|null>(null); 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <Signature />
+      <Signature base64ImageUrl={base64ImageUrl} setBase64ImageUrl={setbase64ImageUrl} />
     </div>
   );
 }
