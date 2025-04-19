c168c760e63b2b113b7f1a1e997006f0975bff63
LittleNa1000 <naphanccnp@gmail.com>
2025-04-19 18:10:48 +0700

    feat: backbutton on signature and photoupload component

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
 
-function BackButton() {
+function BackButton({ onClick }: { onClick?: () => void }) {
   const router = useRouter();
   const pathName = usePathname();
 
@@ -15,10 +15,10 @@ function BackButton() {
 
   return (
     <button
-      onClick={handleClick}
+      onClick={onClick ?? handleClick}
       className={
         isPhotoidRoute
-          ? "absolute left-5 top-5 z-20 flex h-7 w-7 items-center justify-center rounded-full border-2 border-[#3D245B] text-[1.5rem] text-[#3D245B] hover:bg-[#3D245B] hover:text-white"
+          ? "absolute left-5 top-5 z-30 flex h-7 w-7 items-center justify-center rounded-full border-2 border-[#3D245B] text-[1.5rem] text-[#3D245B] hover:bg-[#3D245B] hover:text-white"
           : "absolute left-5 top-5 z-30 flex h-7 w-7 items-center justify-center rounded-full border-2 border-white bg-transparent text-[1.5rem] text-white hover:bg-white hover:text-black"
       }
     >
--- src/components/PhotoUpload/PhotoUpload.tsx
+++ src/components/PhotoUpload/PhotoUpload.tsx
@@ -10,6 +10,7 @@ import BackGround from "../BackgroundPhotoId";
 import SponsorSection from "../SponsorSection/SponsorSection";
 import SocialMediaBar from "../SocialMediaBar/SocialMediaBar";
 import Header from "../Header/Header";
+import BackButton from "../BackButton/BackButton";
 
 const FILE_SIZE_LIMIT = 4 * 1024 * 1024; // 4MB
 
@@ -164,14 +165,7 @@ function PhotoUpload({
         </button>
       )}
 
-      {isModalOpen && (
-        <button
-          className="absolute left-4 top-4 z-30 flex h-10 w-10 items-center justify-center rounded-full border-2 border-[#3D245B]"
-          onClick={closeModal}
-        >
-          ←
-        </button>
-      )}
+      {isModalOpen && <BackButton onClick={closeModal} />}
 
       {/* Modal for Cropping Image */}
       {isModalOpen && (
