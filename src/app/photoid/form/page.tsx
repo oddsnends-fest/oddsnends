6136e17b1a1fc8696766325b7a928772bdfc77b4
future-html <witsanuporn19646@gmail.com>
2025-04-24 11:57:19 +0700

    fix: modify frame photo and photoid Upload

--- src/app/photoid/form/page.tsx
+++ src/app/photoid/form/page.tsx
@@ -1,53 +1,33 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import { Calendar } from "@/components/ui/calendar";
-import { format, set } from "date-fns";
-import { formatDate } from "@/hooks/formatDate";
+import { format } from "date-fns";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import Image from "next/image";
 import React from "react";
 import { ANIMALS } from "@/constants/spirit-animals";
 import { HOBBY } from "@/constants/hobby";
 import Signature from "@/components/Signature";
 import PhotoUpload from "@/components/PhotoUpload/PhotoUpload";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import BackGround from "@/components/BackgroundPhotoId";
 import ImageCanvas from "@/components/BackgroundPhotoId/ImageCanvas";
-// import SliderBox from "@/components/SliderBox/SliderBox";
-// import StarCanvas from "@/image/Frame 13879.png";
 import BackButton from "@/components/BackButton/BackButton";
 
-// import SponsorSection from "@/components/SponsorSection/SponsorSection";
-// import useLocalStorage from "@/hooks/useLocalStorage";
-// import SocialMediaBar from "@/components/SocialMediaBar/SocialMediaBar";
 export default function FormPage() {
   const [name, setName] = useState("");
   const [hobby, setHobby] = useState("");
   const [date, setDate] = useState<Date>();
   const [spiritAnimal, setSpiritAnimal] = useState("");
   const [openCalendar, setOpenCalendar] = useState(false);
-
-  const [isSubmitted, setIsSubmitted] = useState(false); // set to false later
-  // const [isEditData, setIsEditData] = useState(false);
-
   const [base64ImageUrl, setBase64ImageUrl] = useState<string | null>(null);
-
   const [croppedImage, setCroppedImage] = useState<string | null>(null);
 
-  const [isChecking, setIsChecking] = useState(false);
-
-  // const [stepForm, setStepForm] = useState(1);
-
-  const [gotoResultPage, setGotoResultPage] = useState(false);
-
-  // when isModalOpen = true
-
   const router = useRouter();
-
   // console.logle.log(name, hobby, date, spiritAnimal, base64ImageUrl, "information");
   // when submit submit  = true change isSubmitted to true and isSubmitted set timeout in 3 sec and
 
@@ -74,91 +54,58 @@ export default function FormPage() {
       croppedImage,
     };
 
-    // question is how to get the data the other info that I mocked
-    // const formData = {
-    //   user_id: "1", // if don't fill either user_id or email response status 500
-    //   // email: "emailuser@gmail.com",
-    //   display_name: "nameUser",
-    //   full_name: name, // full_name is required
-    //   email: "emailUser@gmail.com", // must follow this format
-    //   // the other one is optional so recommended to have but don't need to
-
-    //   hobby,
-    //   dob: date,
-    //   age: 23, // age is required
-    //   occupation: "occupation",
-    //   channel: "INSTAGRAM",
-    //   // line_profile_pic, phone, gender, photo,
-    //   spirit_animal: spiritAnimal,
-    //   photoid_name: "photoid_name",
-    //   // signature optional hobby
-    // };
-    window.localStorage.setItem("info", JSON.stringify(formData));
-    //submit data
+    window.localStorage.setItem("formData", JSON.stringify(formData));
     // console.logle.log("Form Data Submitted:", formData);
-    // localStorage should set here
 
-    setIsSubmitted(true);
-    setIsChecking(false);
-
-    setName("");
-    setHobby("");
-    setDate(new Date());
-    setSpiritAnimal("");
-    setBase64ImageUrl(null);
-    setCroppedImage(null);
+    router.push("/photoid/recheck");
   };
 
   useEffect(() => {
-    if (isSubmitted && !isChecking) {
-      const timeId = setInterval(() => {
-        setIsSubmitted(false);
+    const frameSelected = localStorage.getItem("frame");
+    const storedData = localStorage.getItem("formData");
+    if (!frameSelected) redirect("/photoid/frame");
+    if (storedData) {
+      try {
+        const parsed = JSON.parse(storedData) as unknown as {
+          name?: string;
+          hobby?: string;
+          date?: string;
+          spiritAnimal?: string;
+          base64ImageUrl?: string;
+          croppedImage?: string;
+        };
 
-        setGotoResultPage(true);
-      }, 3000); // custom time appropiate with video
-
-      return () => clearInterval(timeId); // Proper cleanup
+        setName(parsed.name ?? "");
+        setHobby(parsed.hobby ?? "");
+        setDate(parsed.date ? new Date(parsed.date) : undefined);
+        setSpiritAnimal(parsed.spiritAnimal ?? "");
+        setBase64ImageUrl(parsed.base64ImageUrl ?? "/");
+        setCroppedImage(parsed.croppedImage ?? "/");
+      } catch (error) {
+        console.error("Error parsing formData from localStorage", error);
+      }
     }
-    if (!isSubmitted && !isChecking && gotoResultPage) {
-      router.push("/photoid/result");
-    }
-  }, [isSubmitted, isChecking, gotoResultPage]); // uncomment for design
-
-  // const isFilledAllData =
-  //   name && hobby && date && spiritAnimal && base64ImageUrl && croppedImage;
+  }, []);
 
   return (
-    // when isSubmit is false ==> show form and fill in
-    // when status of gotoresultpage is true
-    // isSubmitted is false
-    // cannot show fill in
-    <div className="overflow-hidden text-[#3D245B]">
-      <button
-        className="absolute left-4 top-4 z-30 flex h-10 w-10 items-center justify-center rounded-full border-2 border-[#3D245B]"
-        onClick={() => {
-          router.back();
-        }}
-      >
-        ←
-      </button>
-      <BackGround />
+    <div className="w-full">
+      <BackButton />
+      <div className="overflow-hidden text-[#3D245B]">
+        <BackGround />
 
-      <ImageCanvas />
-      {!isSubmitted && !gotoResultPage && !isChecking && (
+        <ImageCanvas />
         <div className="mt-5">
           <h1 className="title-photoid">Your info</h1>
           <p className="subtitle-photoid">ใส่ข้อมูลของคุณ</p>
         </div>
-      )}
 
-      {/* name */}
-      {!isSubmitted && !gotoResultPage && !isChecking && (
+        {/* name */}
         <section className="grid gap-4 p-6">
-          <div className="grid grid-cols-2 gap-2">
+          <div className="grid grid-cols-2 gap-3">
             <div>
               <label htmlFor="name" className="mb-2 text-xs font-medium">
                 Name{" "}
-                <span className="font-poppins text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]">
+                <span className="ml-1 text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]/60">
                   ชื่อ
                 </span>
               </label>
@@ -167,7 +114,7 @@ export default function FormPage() {
                 placeholder="ชื่อ"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                className="h-10 w-full rounded-lg p-2 text-sm"
+                className="my-1 w-full rounded-xl p-2 text-sm"
               />
             </div>
 
@@ -175,13 +122,13 @@ export default function FormPage() {
             <div>
               <label htmlFor="hobby" className="mb-2 text-xs font-medium">
                 Hobby{" "}
-                <span className="font-poppins text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]">
+                <span className="ml-1 text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]/60">
                   งานอดิเรก
                 </span>
               </label>
               <select
                 id="hobby"
-                className={`h-10 w-full rounded-md p-2 text-sm ${hobby == "" ? "text-gray-400" : "text-black"}`}
+                className={`my-1 w-full rounded-xl p-2 text-sm ${hobby == "" ? "text-gray-400" : "text-black"}`}
                 value={hobby}
                 onChange={(e) => setHobby(e.target.value)}
               >
@@ -200,25 +147,25 @@ export default function FormPage() {
             <div className="col-span-1">
               <label htmlFor="date" className="mb-2 text-xs font-medium">
                 Date of birth{" "}
-                <span className="font-poppins text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]">
+                <span className="ml-1 text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]/60">
                   ว/ด/ป เกิด
                 </span>
               </label>
-              <div className="flex items-center gap-2">
+              <div className="my-1 flex items-center gap-2">
                 <Popover open={openCalendar} onOpenChange={setOpenCalendar}>
                   <PopoverTrigger asChild>
                     <div
                       onClick={() => setOpenCalendar(true)}
-                      className={`w-full rounded-md bg-white p-2 text-sm ${date ? "text-black" : "text-gray-400"}`}
+                      className={`w-full rounded-xl bg-white p-2 text-sm ${date ? "text-black" : "text-gray-400"}`}
                     >
                       {date ? format(date, "dd/MM/yyyy") : "DD/MM/YYYY"}
                     </div>
                     {/* <Button
-                        variant={"outline"}
-                        className="flex h-10 w-12 items-center justify-center border-2 border-black p-0"
-                      >
-                        <CalendarIcon className="h-5 w-5" />
-                      </Button> */}
+                          variant={"outline"}
+                          className="flex h-10 w-12 items-center justify-center border-2 border-black p-0"
+                        >
+                          <CalendarIcon className="h-5 w-5" />
+                        </Button> */}
                   </PopoverTrigger>
                   <PopoverContent className="w-auto p-0" align="end">
                     <Calendar
@@ -245,12 +192,12 @@ export default function FormPage() {
                 className="mb-2 text-xs font-medium"
               >
                 Your Animal{" "}
-                <span className="text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]">
+                <span className="ml-1 text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]/60">
                   เลือกสัตว์ที่ต้องการ
                 </span>
               </label>
 
-              <ul className="no-scrollbar flex gap-4 overflow-auto">
+              <ul className="no-scrollbar my-1 flex gap-2 overflow-auto">
                 {ANIMALS.map(({ value, label }) => (
                   <li
                     onClick={() => {
@@ -258,7 +205,7 @@ export default function FormPage() {
                     }}
                     key={value}
                     value={value}
-                    className={`flex cursor-pointer items-center justify-center px-2 ${spiritAnimal === value ? "bg-black text-white" : "bg-white text-black"}`}
+                    className={`flex cursor-pointer items-center justify-center rounded-xl px-4 ${spiritAnimal === value ? "bg-black text-white" : "bg-white text-black"}`}
                   >
                     {label}
                   </li>
@@ -266,13 +213,13 @@ export default function FormPage() {
               </ul>
             </div>
           </div>
-          <section className="grid grid-cols-3 gap-6 place-self-start">
+          <section className="mt-3 grid grid-cols-3 gap-6 place-self-start">
             <label
               htmlFor="signature"
               className="col-span-1 mb-2 text-xs font-medium"
             >
               Your signature
-              <p className="text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]">
+              <p className="text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]/60">
                 ลายเซ็น
               </p>
             </label>
@@ -289,7 +236,7 @@ export default function FormPage() {
               className="col-span-1 mb-2 text-xs font-medium"
             >
               Your Photo ID
-              <p className="text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]">
+              <p className="text-xs font-light leading-[100%] tracking-[0] text-[#3D245B]/60">
                 รูปถ่าย
               </p>
             </label>
@@ -305,149 +252,18 @@ export default function FormPage() {
             spiritAnimal &&
             base64ImageUrl &&
             croppedImage && (
-              <div className="relative mt-6 flex items-center justify-center">
+              <div className="relative mb-6 mt-12 flex items-center justify-center">
                 <button
-                  className="absolute w-[16rem] rounded-full bg-purple-gradient py-2 text-[1.25rem] text-white"
-                  onClick={() => {
-                    setIsChecking(true);
-                  }}
+                  className="absolute w-[16rem] rounded-full bg-purple-gradient py-3 text-center font-poppins text-[1.25rem] text-xl font-semibold tracking-wider text-white shadow-lg active:scale-90"
+                  onClick={handleSubmit}
                 >
                   SUBMIT
                 </button>
               </div>
             )}
-
           {/* send button */}
         </section>
-      )}
-
-      {!isSubmitted && !gotoResultPage && isChecking && (
-        <>
-          <div className="relative m-6 bg-white py-2">
-            <button
-              className="absolute right-2 top-2 z-20"
-              onClick={() => {
-                setIsChecking(false);
-              }}
-            >
-              <Image
-                src={"/photoid/eraser.svg"}
-                alt="eraser"
-                width={20}
-                height={20}
-              />
-            </button>
-            <div>
-              <h2 className="text-center font-cooper text-[2.25rem]">
-                Your Info
-              </h2>
-              <p className="text-center">ตรวจสอบข้อมูลของคุณ</p>
-            </div>
-
-            <div className="grid grid-cols-3 p-4">
-              <div className="col-span-2 p-3">
-                <div className="mb-2">
-                  <span>Name</span>
-                  <p className="w-full rounded-sm border border-black px-3 py-1">
-                    {name}
-                  </p>
-                </div>
-                <div className="mb-2">
-                  <span>Hobby</span>
-                  <p className="w-full rounded-sm border border-black px-3 py-1">
-                    {hobby}
-                  </p>
-                </div>
-                <div className="mb-2">
-                  <span>Date</span>
-                  <p className="w-full rounded-sm border border-black px-3 py-1">
-                    {date && formatDate(new Date(date.toISOString()))}
-                  </p>
-                </div>
-                <div className="mb-2">
-                  <span>Signature</span>
-                  <div className="relative h-16 w-full rounded-sm border border-black">
-                    {base64ImageUrl && (
-                      <Image
-                        src={base64ImageUrl}
-                        className="object-contain"
-                        fill={true}
-                        alt="signature"
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className="relative col-span-1 p-3">
-                <div>
-                  <span>Animal</span>
-                  <p>{spiritAnimal}</p>
-                </div>
-                <div>
-                  <span>ID Photo</span>
-                  <div className="relative h-24 w-24 rounded-sm border border-black">
-                    {croppedImage && (
-                      <Image
-                        className="rounded-sm"
-                        src={croppedImage}
-                        fill={true}
-                        alt=""
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            {/* <span>Checking page</span>
-          <button
-            onClick={() => {
-              setIsChecking(false);
-            }}
-          >
-            Cancel (rubber)
-          </button>
-          <button onClick={handleSubmit}>Go to result</button> */}
-          </div>
-          <div className="flex w-full justify-center">
-            <button
-              onClick={handleSubmit}
-              className="w-[16rem] rounded-full bg-purple-gradient py-2 text-[1.25rem] text-white"
-            >
-              LET&#39;S GO!
-            </button>
-          </div>
-        </>
-      )}
-      {/* Printing Page */}
-      {((isSubmitted && !isChecking) ||
-        (!isSubmitted && !isChecking && gotoResultPage)) && (
-        <section className="">
-          <div>
-            <BackButton />
-          </div>
-          <div className="absolute left-1/2 top-1/2 -translate-x-[calc(50%)] -translate-y-[calc(50%+4rem)]">
-            <div className="flex flex-col items-center justify-center gap-6">
-              <h1 className="text-3xl">Printing...</h1>
-              <Image
-                src="/photoid/photo_prints.png"
-                alt="oddie.png"
-                width={90}
-                height={90}
-              />
-              <div className="flex justify-center gap-2">
-                <div className="h-4 w-4 rounded-full bg-[#3D245B]"></div>
-                <div className="h-4 w-4 rounded-full bg-[#3D245B]"></div>
-                <div className="h-4 w-4 rounded-full bg-[#3D245B]"></div>
-              </div>
-              <h2 className="text-2xl">Almost Done!</h2>
-            </div>
-          </div>
-          {/* <div className="absolute bottom-0">
-            <SponsorSection />
-          </div> */}
-        </section>
-      )}
+      </div>
     </div>
   );
 }
--- src/components/PhotoUpload/ImageCropper/ImageCropper.tsx
+++ src/components/PhotoUpload/ImageCropper/ImageCropper.tsx
@@ -1,115 +1,124 @@
-"use client"
+"use client";
 import React from "react";
-import ReactCrop, { centerCrop, makeAspectCrop, type Crop } from 'react-image-crop';
-import 'react-image-crop/dist/ReactCrop.css'
+import ReactCrop, {
+  centerCrop,
+  makeAspectCrop,
+  type Crop,
+} from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
 import Image from "next/image";
 import { useState, useRef, useEffect, useCallback } from "react";
 
 interface ImageCropperProps {
-    src: string;
-    onSaveCrop: (croppedImage: string) => void;
-    confirm: boolean;
+  src: string;
+  onSaveCrop: (croppedImage: string) => void;
+  confirm: boolean;
 }
 
 function ImageCropper({ src, onSaveCrop, confirm }: ImageCropperProps) {
-    const [crop, setCrop] = useState<Crop>();
-    const [croppedArea, setCroppedArea] = useState<Crop | null>(null);
-    const imgRef = useRef<HTMLImageElement | null>(null);
+  const [crop, setCrop] = useState<Crop>();
+  const [croppedArea, setCroppedArea] = useState<Crop | null>(null);
+  const imgRef = useRef<HTMLImageElement | null>(null);
 
-    // set initial crop area to center of the image and aspect ratio 4:5
-    const onImageLoad = (e: React.SyntheticEvent<HTMLImageElement>) => {
-        const { naturalWidth: width, naturalHeight: height } = e.currentTarget;
+  // set initial crop area to center of the image and aspect ratio 4:5
+  const onImageLoad = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    const { naturalWidth: width, naturalHeight: height } = e.currentTarget;
 
-        const crop = centerCrop(
-            makeAspectCrop(
-                {
-                    unit: '%',
-                    width: 90,
-                },
-                4 / 5, // Set the aspect ratio to 4:5
-                width,
-                height
-            ),
-            width,
-            height
-        );
+    const crop = centerCrop(
+      makeAspectCrop(
+        {
+          unit: "%",
+          width: 90,
+        },
+        4 / 5, // Set the aspect ratio to 4:5
+        width,
+        height,
+      ),
+      width,
+      height,
+    );
 
-        setCrop(crop);
+    setCrop(crop);
+  };
+
+  // save crop area data each time the user move / resize the crop area
+  const handleCropComplete = (c: Crop) => {
+    setCroppedArea(c);
+  };
+
+  // helper for useEffect to generate cropped image
+  const generateCroppedImage = useCallback(() => {
+    if (!croppedArea || !imgRef.current) {
+      return;
     }
 
-    // save crop area data each time the user move / resize the crop area
-    const handleCropComplete = (c: Crop) => {
-        setCroppedArea(c);
-    };
+    const canvas = document.createElement("canvas");
+    const scaleX = imgRef.current.naturalWidth / imgRef.current.width;
+    const scaleY = imgRef.current.naturalHeight / imgRef.current.height;
 
-    // helper for useEffect to generate cropped image
-    const generateCroppedImage = useCallback(() => {
-        if (!croppedArea) return;
-        if (!imgRef.current || !croppedArea.width || !croppedArea.height) return;
+    const TARGET_HEIGHT = 100; // 6.25rem = 100px (assuming 1rem = 16px)
+    const TARGET_WIDTH = (TARGET_HEIGHT * scaleY) / scaleX;
 
-        const canvas = document.createElement("canvas");
-        const scaleX = imgRef.current.naturalWidth / imgRef.current.width;
-        const scaleY = imgRef.current.naturalHeight / imgRef.current.height;
+    canvas.width = TARGET_WIDTH;
+    canvas.height = TARGET_HEIGHT;
 
-        canvas.width = croppedArea.width * scaleX;
-        canvas.height = croppedArea.height * scaleY;
+    // canvas.width = croppedArea.width * scaleX;
+    // canvas.height = croppedArea.height * scaleY;
 
-        const ctx = canvas.getContext("2d");
-        if (!ctx) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
-        ctx.drawImage(
-            imgRef.current,
-            croppedArea.x * scaleX,
-            croppedArea.y * scaleY,
-            croppedArea.width * scaleX,
-            croppedArea.height * scaleY,
-            0,
-            0,
-            canvas.width,
-            canvas.height
-        );
-
-        const base64Image = canvas.toDataURL("image/png");
-        onSaveCrop(base64Image); // Call the callback function with the cropped image data
-
-        // canvas.toBlob((blob) => {
-        //     if (!blob) return;
-        //     const previewUrl = URL.createObjectURL(blob);
-        //     setCroppedImage(previewUrl);
-        // }, "image/png");
-
-    }, [croppedArea, onSaveCrop]);
-
-    // Trigger when confirm state from PhotoUpload is true
-    useEffect(() => {
-        if (confirm) {
-            generateCroppedImage();
-        }
-    }, [confirm, generateCroppedImage]);
-
-    return (
-        <>
-            <ReactCrop
-                crop={crop}
-                onChange={(crop, percentCrop) => setCrop(percentCrop)}
-                onComplete={handleCropComplete}
-                aspect={4 / 5}
-                keepSelection={true}
-                // ruleOfThirds={true}
-                className="max-h-[50vh]"
-            >
-                <Image
-                    ref={imgRef}
-                    src={src}
-                    onLoad={onImageLoad}
-                    alt={"Upload Image"}
-                    width={1440}
-                    height={1440}
-                    className="w-auto"
-                />
-            </ReactCrop>
-        </>
+    ctx.drawImage(
+      imgRef.current,
+      croppedArea.x * scaleX,
+      croppedArea.y * scaleY,
+      croppedArea.width * scaleX,
+      croppedArea.height * scaleY,
+      0,
+      0,
+      canvas.width,
+      canvas.height,
     );
+
+    const base64Image = canvas.toDataURL("image/png");
+    onSaveCrop(base64Image); // Call the callback function with the cropped image data
+
+    // canvas.toBlob((blob) => {
+    //     if (!blob) return;
+    //     const previewUrl = URL.createObjectURL(blob);
+    //     setCroppedImage(previewUrl);
+    // }, "image/png");
+  }, [croppedArea, onSaveCrop]);
+  // Trigger when confirm state from PhotoUpload is true
+  useEffect(() => {
+    if (confirm) {
+      generateCroppedImage();
+    }
+  }, [confirm, generateCroppedImage]);
+
+  return (
+    <>
+      <ReactCrop
+        crop={crop}
+        onChange={(crop, percentCrop) => setCrop(percentCrop)}
+        onComplete={handleCropComplete}
+        aspect={4 / 5}
+        keepSelection={true}
+        // ruleOfThirds={true}
+        className="max-h-[50vh]"
+      >
+        <Image
+          ref={imgRef}
+          src={src}
+          onLoad={onImageLoad}
+          alt={"Upload Image"}
+          width={1440}
+          height={1440}
+          className="w-auto"
+        />
+      </ReactCrop>
+    </>
+  );
 }
 
 export default ImageCropper;
