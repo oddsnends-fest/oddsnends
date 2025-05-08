a6ee8a22f6c4c439323fb271dcc6e85fc9df2b11
Max <maxxxx0129@gmail.com>
2025-05-08 19:01:45 +0700

    sed laew

---
+++ src/app/register/roll/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+
+export default function roll(){
+    return (
+        <div className="flex flex-col items-center justify-center h-screen ">
+            {/* Background */}
+            <div className="absolute top-0 h-full w-full bg-[url('/images/background/registerbg.jpg')] bg-center bg-cover -z-10"></div>
+            <div>
+                <h1 className="text-5xl font-cooper text-center text-[#3D245B]">Ready to roll?</h1>
+            </div>
+            <div>
+                <p className="text-[#1C1026]">Make it memorable at Odds & Ends!</p>
+            </div>
+            <div>
+                <Image
+                    src="/images/spirit-animals/OTTER.png"
+                    width={300}
+                    height={300}
+                    alt="Picture of the author"
+                />
+            </div>
+            <div>
+                <p className=" bg-gradient-to-b from-[#B56A95] to-[#553B82] rounded-3xl  p-3 text-white">EVENT INFORMATION</p>
+            </div>
+        </div>
+    )
+}
