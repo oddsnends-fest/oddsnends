c480cb2b35f41966835d750d78b003b61a6fbb0d
wnmay <wanathachat@gmail.com>
2025-05-08 20:24:16 +0700

    feat: binance page

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -20,7 +20,7 @@ function ContactSection({ contacts, className = "" }: ContactSectionProps) {
       <div className="text-custom-yellow mt-2 text-center font-cooper text-xl tracking-wider">
         Contact
       </div>
-      <div className="mx-auto flex max-w-[280px] flex-wrap items-center justify-center gap-2">
+      <div className="mx-auto flex flex-wrap items-center justify-center gap-2">
         {contacts.map((contact, index) => (
           <a key={index} className="flex items-center" href={contact.href}>
             <Image
